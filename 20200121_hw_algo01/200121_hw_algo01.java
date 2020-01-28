ce7faa41d15930dba8b457d7f67f6f12739a6555
성열호 <dufgh1009@naver.com>
2020-01-28 00:08:19 +0000

    200121_hw_algo01 - 최빈수 구하기

---
+++ 20200121_hw_algo01/200121_hw_algo01.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+ 
+public class Solution {
+ 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+ 
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+ 
+         
+         
+         
+        for(int test_case = 1; test_case <= T; test_case++){
+             
+            int[] a = new int[101];
+            int max = 0;
+            int b = 0;
+             
+            sc.nextInt();
+             
+             
+            for(int i =0; i < 1000; i++) {
+                 
+                int idx = sc.nextInt();
+                a[idx]++;
+                 
+                for(int j = 0; j < a.length; j++) {
+                    if(a[j] > max) {
+                        max = a[j];
+                        b = j;
+                    }else if(a[j] == max) {
+                        if(j > b) {
+                            b = j;
+                        }
+                    }
+                }
+            }
+             
+             
+            System.out.println("#" + test_case + " " + b);
+        }
+    }
+}
