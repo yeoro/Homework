9231284cd048dfde2fdb587cfd407b2991777e04
성열호 <dufgh1009@naver.com>
2020-01-29 09:33:16 +0000

    AlpaTest1

---
+++ 20200122_ws_java1/AlpaTest1.java
@@ -0,0 +1,13 @@
+package com.ssafy.java;
+
+public class AlpaTest1 {
+
+	public static void main(String[] args) {
+		System.out.println('A');
+		System.out.println(String.format("%c %c", 'B', 'C'));
+		System.out.println(String.format("%c %c %c", 'D', 'E', 'F'));
+		System.out.println(String.format("%c %c %c %c", 'G', 'H', 'I', 'J'));
+		System.out.println(String.format("%c %c %c %c %c", 'K', 'L', 'N', 'M', 'O'));
+	}
+
+}
